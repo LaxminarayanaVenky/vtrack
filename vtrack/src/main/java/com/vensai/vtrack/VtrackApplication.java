9ae63b9994ff41a5c688a775cf070da3f707d4c2
Saikoti <saikoti.r.143@gmail.com>
2023-06-22 18:22:20 +0530

    Initial Commit for the project

---
+++ vtrack/src/main/java/com/vensai/vtrack/VtrackApplication.java
@@ -0,0 +1,13 @@
+package com.vensai.vtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtrackApplication.class, args);
+	}
+
+}
